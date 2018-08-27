9c5d9b52a2eab45bea20eda9ca3b747bd5eee0ae
tkn <tyson_ngo@hotmail.com>
2018-08-27 13:27:42 -0400

    blacklist

--- commands/index.js
+++ commands/index.js
@@ -13,10 +13,10 @@ module.exports = bot => {
 			helpDescriptions.push(cmd.description);
 			bot.on('message', msg => {
 				if (msg.author.bot || msg.channel.type === 'dm') return;
-				if (cmd.blacklist && ~cmd.blacklist.indexOf(msg.author.id)){
-					return msg.reply('you cannot use this command.');
-				}
 				if (cmd.cmdRe.test(msg.content)){
+					if (cmd.blacklist && ~cmd.blacklist.indexOf(msg.author.id)){
+						return msg.reply('you cannot use this command.');
+					}
 					cmd.exec(msg);
 				}
 			})
