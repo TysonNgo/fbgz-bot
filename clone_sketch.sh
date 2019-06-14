6563881ce2c5140f7d9678fa59596f288bd23b18
T <tyson_ngo@hotmail.com>
2019-06-14 16:55:51 -0400

    clone daily sketch script

---
+++ clone_sketch.sh
@@ -0,0 +1,5 @@
+git clone git@github.com:TysonNgo/Discord-Daily-Sketch-Bot.git
+git clone git@github.com:TysonNgo/Discord-Daily-Sketch-Bot.git Discord-Daily-Sketch-Bot-MAL-Char
+
+cd Discord-Daily-Sketch-Bot-MAL-Char
+git checkout mal-char
