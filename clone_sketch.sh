bfd9b0dc25f8e0295efd04cc06a26377502369a1
T <tyson_ngo@hotmail.com>
2019-06-14 17:03:54 -0400

    modify clone script

--- clone_sketch.sh
+++ clone_sketch.sh
@@ -1,5 +1,3 @@
 git clone git@github.com:TysonNgo/Discord-Daily-Sketch-Bot.git
-git clone git@github.com:TysonNgo/Discord-Daily-Sketch-Bot.git Discord-Daily-Sketch-Bot-MAL-Char
+git clone -b mal-char git@github.com:TysonNgo/Discord-Daily-Sketch-Bot.git Discord-Daily-Sketch-Bot-MAL-Char
 
-cd Discord-Daily-Sketch-Bot-MAL-Char
-git checkout mal-char
